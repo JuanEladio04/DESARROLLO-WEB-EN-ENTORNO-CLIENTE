1b6198832647905ef2414adb39f4ead3a8f848ee
JuanEladio04 <juaneladiopm@gmail.com>
2024-01-22 11:05:08 +0100

    ejemplo

--- Tema9/Proyecto1/src/app/app.module.ts
+++ Tema9/Proyecto1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    Componente1Component,
+    Componente2Component,
   ],
   imports: [
     BrowserModule,
---
+++ Tema9/Proyecto1/src/app/componente1/componente1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente1',
+  templateUrl: './componente1.component.html',
+  styleUrl: './componente1.component.css'
+})
+export class Componente1Component {
+
+}
